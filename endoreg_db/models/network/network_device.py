e00cb9aa67a36c7c063cbf982e0445841d28845c
maddonix <tlux14@googlemail.com>
2024-08-10 18:38:41 +0200

    added ping function to update online status of netDevices

--- endoreg_db/models/network/network_device.py
+++ endoreg_db/models/network/network_device.py
@@ -12,6 +12,7 @@ class NetworkDevice(models.Model):
     ip = models.GenericIPAddressField(blank=True, null=True)
     description = models.CharField(max_length=255)
     device_type = models.ForeignKey("NetworkDeviceType", on_delete=models.CASCADE)
+    online = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     objects = NetworkDeviceManager()
@@ -25,3 +26,28 @@ class NetworkDevice(models.Model):
         
     def natural_key(self):
         return (self.name,)
+    
+    def ping(self):
+        target_ip = self.ip
+
+        # Import the required module
+        import subprocess
+
+        # Define the command
+        command = ['ping', '-c', '1', target_ip]
+
+        # Run the command
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Wait for the process to terminate
+        process.wait()
+
+        # Check the return code
+        return_code = process.returncode
+        
+        # Return True if the return code is 0, False otherwise
+        self.online = return_code == 0
+        self.save()
+        return self.online
+    
+
