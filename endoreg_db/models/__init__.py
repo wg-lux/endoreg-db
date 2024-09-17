b1a3c7c37e28211d822628ee8a5ef660b8c52865
maxhild <Maxhild10@gmail.com>
2024-09-17 13:51:51 +0200

    Failed Import solved

--- endoreg_db/models/__init__.py
+++ endoreg_db/models/__init__.py
@@ -43,6 +43,8 @@ from .annotation import (
     ImageClassificationAnnotation,
     LegacyBinaryClassificationAnnotationTask,
     BinaryClassificationAnnotationTask,
+    AnonymizedFile,
+    UploadedFile,
 )
 
 from .legacy_data import (
--- endoreg_db/models/annotation/__init__.py
+++ endoreg_db/models/annotation/__init__.py
@@ -1,3 +1,3 @@
 from .image_classification import ImageClassificationAnnotation
 from .binary_classification_annotation_task import LegacyBinaryClassificationAnnotationTask, BinaryClassificationAnnotationTask
-from .anonymized_image_annotation import AnonymousImageAnnotation, DroppedName, AnonymizedImageLabel
+from .anonymized_image_annotation import AnonymousImageAnnotation, DroppedName, AnonymizedImageLabel, AnonymizedFile, UploadedFile
