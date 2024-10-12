1a39f1320e1b08dfc0c4bd69c65705b6b87544c8
maddonix <tlux14@googlemail.com>
2024-10-12 17:19:03 +0200

    implement openvpn log attributes

--- endoreg_db/models/logging/network_device.py
+++ endoreg_db/models/logging/network_device.py
@@ -8,6 +8,13 @@ class NetworkDeviceLogEntry(AbstractLogEntry):
     device = models.ForeignKey("NetworkDevice", on_delete=models.CASCADE)
     log_type = models.ForeignKey("LogType", on_delete=models.CASCADE, null=True, blank=True)
 
+    datetime = models.DateTimeField(default=None, null=True, blank=True)
+    vpn_service_status = models.BooleanField(default=False)
+    vpn_service_restart_attempt = models.BooleanField(default=False)
+    vpn_service_restart_success = models.BooleanField(null=True, blank=True)
+    ping_vpn = models.BooleanField(default=False)
+    ping_www = models.BooleanField(default=False)
+
     class Meta:
         verbose_name = "Network Device Log Entry"
         verbose_name_plural = "Network Device Log Entries"
