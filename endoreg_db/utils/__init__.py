df352dec23e2406ff512c0159757d1e1f47d5d87
maddonix <tlux14@googlemail.com>
2025-02-16 20:45:13 +0100

    migrate dbconfig

--- endoreg_db/utils/__init__.py
+++ endoreg_db/utils/__init__.py
@@ -1,3 +1,12 @@
+'''Module for utility classes and functions.'''
 from .dataloader import load_model_data_from_yaml
 from .parse_and_generate_yaml import collect_center_names
 from .validate_endo_roi import validate_endo_roi
+from .pydantic_models import DbConfig
+
+__all__ = [
+    'load_model_data_from_yaml',
+    'collect_center_names',
+    'validate_endo_roi',
+    'DbConfig'
+]
---
+++ endoreg_db/utils/pydantic_models/__init__.py
@@ -0,0 +1,6 @@
+'''Module for Pydantic models.'''
+from .db_config import DbConfig
+
+__all__ = [
+    'DbConfig'
+]
