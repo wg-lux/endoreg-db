c4fe3ca19c969a3512e19c4d8c9591d79675d3f5
maddonix <tlux14@googlemail.com>
2025-08-25 08:53:45 +0200

    rm old serializers

--- endoreg_db/serializers/_old/frame.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from endoreg_db.models import Frame
-
-class FrameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Frame
-        fields = '__all__'
-
-
--- endoreg_db/serializers/_old/sync/prediction_meta.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from endoreg_db.models import VideoPredictionMeta
-
-class VideoPredictionMetaSerializer(serializers.Serializer):
-    """Serializer for PredictionMeta representation."""
-
-    class Meta:
-        model = VideoPredictionMeta
-        fields = []
