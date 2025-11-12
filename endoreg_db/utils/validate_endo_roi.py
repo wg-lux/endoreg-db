d8dcef15827084073e675fcc402a6a04450b4511
maddonix <tlux14@googlemail.com>
2025-11-12 13:23:22 +0100

    improve video anonymization test coverage

--- endoreg_db/utils/validate_endo_roi.py
+++ endoreg_db/utils/validate_endo_roi.py
@@ -1,9 +1,15 @@
-def validate_endo_roi(endo_roi_dict:dict):
+from typing import Dict, Union
+
+
+def validate_endo_roi(endo_roi_dict: Union[Dict[str, int], None]):
     """
     Validate endoscope ROI dictionary. The dictionary must have the following
     keys: x, y, width, height. The values must be greater than or equal to 0
     for x and y, and greater than 0 for width and height.
     """
+    if not endo_roi_dict:
+        return False
+
     for key, value in endo_roi_dict.items():
         if key == "x":
             assert value >= 0, f"Endoscope ROI x value must be greater than or equal to 0. Got {value}"
