40cf98ee39dd08336025b06704a9cf6a1f0066d7
maddonix <tlux14@googlemail.com>
2025-11-27 10:38:39 +0100

    minor test fixes

--- tests/administration/ai/test_create_model_meta.py
+++ tests/administration/ai/test_create_model_meta.py
@@ -3,27 +3,19 @@ from pathlib import Path
 import pytest
 from django.test import TestCase
 
-from ...helpers.data_loader import (
-    load_ai_model_label_data,
-    load_ai_model_data,
-    load_default_ai_model
-)
+from endoreg_db.models import ModelMeta
 
+from ...helpers.data_loader import load_data
 from ...helpers.default_objects import (
     get_latest_segmentation_model,
 )
 
-from endoreg_db.models import ModelMeta
 
 class AiModelTest(TestCase):
     def setUp(self):
-
-        load_ai_model_label_data()
-        load_ai_model_data()
-        load_default_ai_model()
+        load_data()
 
         self.ai_model_meta = get_latest_segmentation_model()
-
 
     def test_model_meta_creation(self):
         """Test the creation of an AiModel instance."""
@@ -34,8 +26,7 @@ class AiModelTest(TestCase):
 @pytest.mark.django_db(transaction=True)
 def test_setup_default_model_meta_from_huggingface_downloads_safetensors():
     """Ensure we can pull safetensor weights from Hugging Face and register metadata."""
-    load_ai_model_label_data()
-    load_ai_model_data()
+    load_data()
 
     ModelMeta.objects.all().delete()
 
