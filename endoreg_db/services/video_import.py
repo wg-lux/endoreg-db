e466d541e109d3454bc40a0ff2356f9f3365b765
maxhild <maxhild10@gmail.com>
2025-12-17 14:15:18 +0100

    Typing improvements

--- endoreg_db/services/video_import.py
+++ endoreg_db/services/video_import.py
@@ -1,10 +1,9 @@
 from endoreg_db.import_files.video_import_service import VideoImportService as vis
-from endoreg_db.models import VideoFile
-from pathlib import Path
+
 
 class VideoImportService(vis):
-    """
-    Wrapper for service import of concurrency safe VideoImportService, 
-    here celery will be implemented in the future.
-    12.02.2025
-    """
+    __doc__ = (vis.__doc__ or "") + (
+        "Wrapper for service import of concurrency safe VideoImportService.\n"
+        "Implemented for future Celery integration (12.02.2025).\n\n"
+    )
+    pass
