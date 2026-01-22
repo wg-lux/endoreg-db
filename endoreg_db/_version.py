2817942fe877f2978239b1ea0e152ca008b3b3d2
maxhild <maxhild10@gmail.com>
2026-01-22 23:43:49 +0100

    new version

--- endoreg_db/_version.py
+++ endoreg_db/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.8.9.30.dev0+gf0b9d3bfe.d19800101'
-__version_tuple__ = version_tuple = (0, 8, 9, 30, 'dev0', 'gf0b9d3bfe.d19800101')
+__version__ = version = '0.8.9.29'
+__version_tuple__ = version_tuple = (0, 8, 9, 31, 'dev0', 'g078ed6bf1.d19800101')
 
 __commit_id__ = commit_id = None
