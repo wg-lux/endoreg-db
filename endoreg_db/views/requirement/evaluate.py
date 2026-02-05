06e1edee3beef5ab85ebbc242af17a6c6638dfb5
Thomas <hamza.ukw@gmail.com>
2026-02-05 12:37:15 +0100

    issue 257

--- endoreg_db/views/requirement/evaluate.py
+++ endoreg_db/views/requirement/evaluate.py
@@ -8,6 +8,11 @@ from endoreg_db.models.medical.patient.patient_examination import PatientExamina
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
 import json
 import logging
 
@@ -15,6 +20,7 @@ logger = logging.getLogger(__name__)
 
 
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def evaluate_requirements(request):
     """
     Evaluate requirements (all selected sets) and always return 200 with structured results.
