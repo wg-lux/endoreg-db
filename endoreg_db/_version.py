c92f543f3e8b605e7d3a5808863c788a96e08651
maxhild <maxhild10@gmail.com>
2026-03-09 16:13:29 +0100

    Report streaming

--- endoreg_db/_version.py
+++ endoreg_db/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.9.0.7.dev0+gb3e9b60c7.d19800101"
-__version_tuple__ = version_tuple = (0, 9, 0, 7, "dev0", "gb3e9b60c7.d19800101")
+__version__ = version = "0.9.0.7"
+__version_tuple__ = version_tuple = (0, 9, 0, 7)
 
 __commit_id__ = commit_id = None
